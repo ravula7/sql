71228a094fc4d0269cc4e722d7ac8e0d1708ec26
ravula7 <ravula7@ad.unc.edu>
2023-07-26 14:33:46 -0400

    commit

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -3,6 +3,7 @@ WSGI config for myproject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
+check commit
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
